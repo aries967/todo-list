9db582522496b11a965c6df3cef7d10bd259b53b
aries967 <ariessatriapratama@gmail.com>
2025-05-18 13:37:54 +0700

    feat: delete todo item

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const form = document.getElementById("todo-form");
 const input = form.querySelector("input");
 const list = document.getElementById("todo-list");
+let todoItems = JSON.parse(localStorage.getItem("items")) || [];
 
-const addTodo = (value) => {
+window.addEventListener("load", () => {
+    todoItems.forEach(item => addTodoItem(item))
+})
+
+const addTodoItem = (value) => {
     let item = document.createElement("li");
-    let html = `<input type="checkbox" name="" id="">${value}<button>x</button>`;
-    item.innerHTML = html;
+    item.textContent = value;
+    _createDeleteButton(item)
     list.appendChild(item);
+}
+
+const _createDeleteButton = (parent) => {
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "x";
+    deleteButton.addEventListener("click", () => {
+        parent.remove();
+        storeList();
+    });
+    parent.appendChild(deleteButton)
+}
+
+const storeList = () => {
+    localStorage.setItem("items", JSON.stringify(todoItems));
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (input.value != "") {
-        addTodo(input.value);
-        input.value = ""
+        addTodoItem(input.value);
+        todoItems.push(input.value)
+        storeList(input.value);
+        input.value = "";
     }
 })
