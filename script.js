2ba381c0739e33f0887269dfce44884f1bec4910
aries967 <ariessatriapratama@gmail.com>
2025-05-21 12:45:31 +0700

    style: use dot for property access, adding whitespaces for readability

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const form = document.getElementById("todo-form");
 const input = form.querySelector("input");
 const list = document.getElementById("todo-list");
-let todoList = JSON.parse(localStorage.getItem("todoList")) || {items: [], idCounter: 0};
+let todoList = JSON.parse(localStorage.getItem("todoList")) || { items: [], idCounter: 0 };
 
 window.addEventListener("load", () => {
-    todoList["items"].forEach(item => addTodoItem(item.id, item.name, item.completed))
-})
+    todoList.items.forEach(item => addTodoItem(item.id, item.name, item.completed))
+});
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (input.value != "") {
-        addTodoItem(todoList["idCounter"] + 1, input.value);
-        todoList["items"].push({id: todoList["idCounter"]+1, name: input.value, completed: false})
-        todoList["idCounter"] += 1;
+        addTodoItem(todoList.idCounter + 1, input.value);
+        todoList.items.push({ id: todoList.idCounter + 1, name: input.value, completed: false })
+        todoList.idCounter += 1;
         storeList();
         input.value = "";
     }
 });
 
-const addTodoItem = (id, value, completed=false) => {
+const addTodoItem = (id, value, completed = false) => {
     let item = document.createElement("li");
     item.dataset.id = id;
     _createTodoCheckbox(item, completed);
     _createTodoName(item, value);
     _createReorderButtons(item);
-    _createReorderButtons(item, to="bottom");
+    _createReorderButtons(item, to = "bottom");
     _createEditButton(item)
     _createDeleteButton(item);
     list.appendChild(item);
@@ -37,7 +37,7 @@ const _createDeleteButton = (parent) => {
     deleteButton.classList.add("delete-button")
     deleteButton.addEventListener("click", () => {
         parent.remove();
-        todoList["items"] = todoList["items"].filter(item => item.id != parent.dataset.id);
+        todoList.items = todoList.items.filter(item => item.id != parent.dataset.id);
         storeList();
     });
     parent.appendChild(deleteButton)
@@ -56,7 +56,7 @@ const _createTodoCheckbox = (parent, checked) => {
     checkbox.setAttribute("type", "checkbox");
     if (checked) checkbox.checked = true;
     checkbox.addEventListener("change", () => {
-        todoList["items"] = todoList["items"].map(item => item.id == parent.dataset.id ? {...item, completed: checkbox.checked} : item);
+        todoList.items = todoList.items.map(item => item.id == parent.dataset.id ? { ...item, completed: checkbox.checked } : item);
         storeList();
     })
     parent.appendChild(checkbox);
@@ -70,7 +70,7 @@ const _createEditButton = (parent) => {
 
     input.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
-            todoList["items"] = todoList["items"].map(item => item.id == parent.dataset.id ? {...item, name: input.value} : item);
+            todoList.items = todoList.items.map(item => item.id == parent.dataset.id ? { ...item, name: input.value } : item);
             input.disabled = true;
             isEditing = false;
             button.innerHTML = "EDIT";
@@ -83,7 +83,7 @@ const _createEditButton = (parent) => {
         isEditing = !isEditing;
         button.innerHTML = isEditing ? "CANCEL" : "EDIT";
         if (!isEditing) {
-            input.value = todoList["items"].filter(item => item.id == parent.dataset.id)[0]["name"];
+            input.value = todoList.items.filter(item => item.id == parent.dataset.id)[0]["name"];
         }
         if (input.disabled == false) input.focus();
     })
@@ -92,7 +92,7 @@ const _createEditButton = (parent) => {
     parent.appendChild(button);
 }
 
-const _createReorderButtons = (parent, to="top") => {
+const _createReorderButtons = (parent, to = "top") => {
     let button = document.createElement("button");
     if (to === "top") {
         button.innerHTML = "^"
@@ -103,20 +103,20 @@ const _createReorderButtons = (parent, to="top") => {
     button.addEventListener("click", () => {
         console.log(parent, parent.previousSibling)
         let parentCopy = parent.cloneNode(true);
-        let index = todoList["items"].findIndex((item) => item.id == parent.dataset.id)
+        let index = todoList.items.findIndex((item) => item.id == parent.dataset.id)
         console.log(index)
         if (to === "top" && index !== 0) {
             parent.previousSibling.before(parent);
-            let temp = todoList["items"][index - 1];
-            todoList["items"][index - 1] = todoList["items"][index];
-            todoList["items"][index] = temp;
+            let temp = todoList.items[index - 1];
+            todoList.items[index - 1] = todoList.items[index];
+            todoList.items[index] = temp;
             storeList()
         };
         if (to === "bottom" && index !== todoList.length - 1) {
             parent.nextSibling.after(parent);
-            let temp = todoList["items"][index + 1];
-            todoList["items"][index + 1] = todoList["items"][index];
-            todoList["items"][index] = temp;
+            let temp = todoList.items[index + 1];
+            todoList.items[index + 1] = todoList.items[index];
+            todoList.items[index] = temp;
             storeList()
         }
     })
